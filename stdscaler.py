f9e258b455f9bfdf7c18f047c1db2a7ebfea8582
raghav810 <56407650+raghav810@users.noreply.github.com>
2021-05-24 14:39:31 +0530

    Add files via upload

---
+++ stdscaler.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 10 14:32:59 2020
+
+@author: Raghav Khanna
+"""
+
+
+datecolumn = dataset['DATE']
+#dataset.drop('DATE',axis = 1,inplace = True)
+
+#names = dataset.columns# Create the Scaler object
+#scaler = preprocessing.StandardScaler()# Fit your data on the scaler object
+#scaled_df = scaler.fit_transform(dataset)
+#scaled_df = pd.DataFrame(scaled_df, columns=names)
+
+#scaled_df.insert(0, 'DATE', datecolumn)
+#dataset = scaled_df
+
